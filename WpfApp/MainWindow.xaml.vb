e3593dd790ae7d69e8807c80953487215f807ae5
Kelvin Lieu <kelvinlieu12345@gmail.com>
2020-10-11 10:41:13 +0800

    first commit

---
+++ WpfApp/MainWindow.xaml.vb
@@ -0,0 +1,19 @@
+﻿Class MainWindow
+    Private Sub radioButton1_Checked(sender As Object, e As RoutedEventArgs) Handles btnA.Checked
+
+    End Sub
+
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        If btnA.IsChecked = True Then
+            MessageBox.Show("A checked")
+        ElseIf btnB.IsChecked = True Then
+            MessageBox.Show("B checked")
+
+
+        End If
+    End Sub
+
+    Private Sub radioButton_Checked(sender As Object, e As RoutedEventArgs) Handles btnB.Checked
+
+    End Sub
+End Class
